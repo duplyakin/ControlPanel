a9c82937902347e7895ba4ac89cd3b4be864df28
Glebus <zaborovskij.gleb@gmail.com>
2018-04-29 23:12:02 +0300

    Initial commit

---
+++ src/main/java/com/sbt/test/ControlPanel/ControlPanelApplication.java
@@ -0,0 +1,12 @@
+package com.sbt.test.ControlPanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlPanelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sbt/test/ControlPanel/ControlPanelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sbt.test.ControlPanel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControlPanelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
