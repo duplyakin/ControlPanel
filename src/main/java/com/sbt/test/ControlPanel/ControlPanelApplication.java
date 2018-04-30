6ddbbdc6bc8f748be3464ed7882f07d89d6320d7
Glebus <zaborovskij.gleb@gmail.com>
2018-04-30 20:03:55 +0300

    preparations: * added basic thymeleaf views * added config-file for spring security * change base package for bean scanning * added actuator dependency etc.

--- src/main/java/com/sbt/test/ControlPanel/ControlPanelApplication.java
+++ src/main/java/com/sbt/test/ControlPanel/ControlPanelApplication.java
@@ -2,8 +2,10 @@ package com.sbt.test.ControlPanel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.sbt.test")
 public class ControlPanelApplication {
 
 	public static void main(String[] args) {
