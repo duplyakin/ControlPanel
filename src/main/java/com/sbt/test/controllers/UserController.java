4850a23ce0a937e9066b70b68172476e6bcce857
Glebus <zaborovskij.gleb@gmail.com>
2018-05-06 19:49:26 +0300

    pre-alpha: * updated controller methods * added remove-method to repository

--- src/main/java/com/sbt/test/controllers/UserController.java
+++ src/main/java/com/sbt/test/controllers/UserController.java
@@ -1,28 +1,54 @@
 package com.sbt.test.controllers;
 
 import com.sbt.test.entities.User;
+import com.sbt.test.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
+@Slf4j
 public class UserController {
 
-    @GetMapping("/getUser/{id}")
-    @PreAuthorize("hasRole('USER')")
-    public User get(@PathVariable("id") int id) {
-        return User.builder().username("123").build();
+    private final UserRepository repo;
+
+    @Autowired
+    public UserController(UserRepository repo) {
+        this.repo = repo;
     }
 
-    @PutMapping("/addUser")
+    @GetMapping("/get/{username}")
     @PreAuthorize("hasRole('USER')")
-    public User add(@RequestBody User user) {
-        return user;
+    public ResponseEntity<User> get(@PathVariable("username") String username) {
+        try {
+            return repo.getByUsername(username)
+                    .map(ResponseEntity::ok)
+                    .orElseGet(() -> ResponseEntity.status(404).build());
+        } catch (RuntimeException e) {
+            log.error("Something really bad happened on getUser operation:", e);
+            return ResponseEntity.status(412).build();
+        }
     }
 
-    @DeleteMapping("/deleteUser/{id}")
+    @PutMapping("/add")
     @PreAuthorize("hasRole('USER')")
-    public User delete(@PathVariable("id") int id) {
-        return User.builder().username("123").build();
+    public ResponseEntity<User> add(@RequestBody User user) {
+        try {
+            return ResponseEntity.ok(repo.saveAndFlush(user));
+        } catch (RuntimeException e) {
+            log.error("Something really bad happened on addUser operation:", e);
+            return ResponseEntity.status(412).build();
+        }
+    }
+
+    @DeleteMapping("/delete/{username}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<User> delete(@PathVariable("username") String username) {
+        repo.deleteByUsername(username);
+        return ResponseEntity.ok(null);
     }
 
 
--- src/main/java/com/sbt/test/repository/UserRepository.java
+++ src/main/java/com/sbt/test/repository/UserRepository.java
@@ -10,4 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> getByUsername(String userName);
+
+    void deleteByUsername(String username);
+
 }
