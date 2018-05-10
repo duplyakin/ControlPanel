33ae2b30a9e0cf40a7f76efb9cda84a54ac7b089
Glebus <zaborovskij.gleb@gmail.com>
2018-05-11 02:40:17 +0300

    static: * added react-js project as a submodule * temporally skip spring-boot-maven-plugin (static is not a spring boot project)

---
+++ ControlPanelStatic/src/main/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default ReactDOM.render(
+<div>HAYYYYYYYYYYYYYYYY</div>,
+    document.getElementById('react')
+);
---
+++ ControlPanelStatic/webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        main: './src/main/js/index.js'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'SERVER_PATH': JSON.stringify("localhost:8090")
+        })
+    ],
+    output: {
+        filename: 'main.js',
+        path: __dirname + "/src/main/resources/static/built",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$|jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                }
+            }
+        ],
+    },
+    devServer: {
+        contentBase: __dirname + "/src/main/resources/",
+        inline: true,
+        port: 9090
+    },
+};
