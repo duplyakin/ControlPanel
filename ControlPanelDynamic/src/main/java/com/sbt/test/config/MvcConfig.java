c7e096218a6a4f82088f39aba6cb5a08e933cefb
Glebus <zaborovskij.gleb@gmail.com>
2018-05-12 01:13:10 +0300

    dynamic: * added dependency on static to use index.html

--- ControlPanelDynamic/src/main/java/com/sbt/test/config/MvcConfig.java
+++ ControlPanelDynamic/src/main/java/com/sbt/test/config/MvcConfig.java
@@ -9,8 +9,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
     }
 
 }
--- ControlPanelDynamic/src/main/java/com/sbt/test/config/WebSecurityConfig.java
+++ ControlPanelDynamic/src/main/java/com/sbt/test/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home")
+                .antMatchers("/", "/index")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
