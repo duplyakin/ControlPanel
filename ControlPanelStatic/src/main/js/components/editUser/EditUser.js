eefb7cc6c6dbd96449a1235a49f4808ea4c12593
Glebus <zaborovskij.gleb@gmail.com>
2018-05-15 01:59:43 +0300

    static: * first http request was sent to backend (and received by it) * still working on receiving response correctly

--- ControlPanelStatic/src/main/js/components/editUser/EditUser.js
+++ ControlPanelStatic/src/main/js/components/editUser/EditUser.js
@@ -3,8 +3,37 @@ import Button from "react-bootstrap/es/Button";
 import Well from "react-bootstrap/es/Well";
 import _ from "lodash";
 
-export const EditUser = (props) =>
-    <div>
-        <Well>Hi! It's user {_.get(props, "match.params.username", "")} edit form!</Well>
-        <Button>Edit user</Button>
-    </div>;
+export class EditUser extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: 'empty',
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8090/users/get/user', {
+            method: "GET",
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            credentials: 'include',
+            mode: "no-cors",
+            redirect: "follow",
+        }).then(response => {
+            console.log(response);
+            this.setState({user: response})});
+    }
+
+    render() {
+        return <div>
+            <Well>Hi! It's user {_.get(this.props, "match.params.username", "")} edit form!</Well>
+            <Well>{this.state.user.toString()}</Well>
+            <Button>Edit user</Button>
+        </div>
+    }
+
+}
