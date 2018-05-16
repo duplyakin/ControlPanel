934e04ab1dea3807599dd6316bdbaafbe787204b
Glebus <zaborovskij.gleb@gmail.com>
2018-05-17 01:02:14 +0300

    static: * made CreateUser form stub * made basic controls * ensured csrf is on

--- ControlPanelStatic/src/main/js/components/createUser/CreateUser.js
+++ ControlPanelStatic/src/main/js/components/createUser/CreateUser.js
@@ -1,51 +1,96 @@
 import React from 'react';
-import Button from "react-bootstrap/es/Button";
-import Well from "react-bootstrap/es/Well";
-import {Col, ControlLabel, Form, FormControl, FormGroup, HelpBlock} from "react-bootstrap";
+import {TextBox} from '../basic/TextBox'
+import {Button, Col, Form, FormGroup, Well} from "react-bootstrap";
+import {SelectBox} from "../basic/SelectBox";
+
+function getCookie(name) {
+    if (!document.cookie) {
+        return null;
+    }
+
+    const xsrfCookies = document.cookie.split(';')
+        .map(c => c.trim())
+        .filter(c => c.startsWith(name + '='));
+
+    if (xsrfCookies.length === 0) {
+        return null;
+    }
+
+    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+}
 
 export class CreateUser extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                username: "",
+                password: "",
+                roles: ["USER"],
+                privileges: ["READ"],
+                accountNonExpired: true,
+                accountNonLocked: true,
+                credentialsNonExpired: true,
+                enabled: true
+            }
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.addUser = this.addUser.bind(this);
+    }
+
+    handleChange(change, event) {
+        const toChange = this.state.user;
+        toChange[change] = event.target.value;
+        this.setState({user: toChange});
+    }
+
+    addUser() {
+        const csrfToken = getCookie('XSRF-TOKEN');
+
+        fetch('http://localhost:8090/users/add', {
+            method: "PUT",
+            headers:{
+                'Content-Type': 'application/json',
+                'X-XSRF-TOKEN': csrfToken
+            },
+            credentials: "include",
+            redirect: "follow",
+            mode: "cors",
+            body: JSON.stringify(this.state.user)
+        }).then(response => response.json())
+            .then(responseJson => {
+                this.setState({user: responseJson})
+            });
+    }
+
     render() {
+        console.log(this.state);
         return <div>
             <Well>Hi! It's user create form!</Well>
+
             <Form horizontal>
-                <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} sm={1}>Username</Col>
-                    <Col sm={2}>
-                        <FormControl type="text" placeholder="Username" />
-                    </Col>
-                </FormGroup>
 
-                <FormGroup controlId="formHorizontalPassword">
-                    <Col componentClass={ControlLabel} sm={1}>Password</Col>
-                    <Col sm={2}>
-                        <FormControl type="password" placeholder="Password" />
-                    </Col>
-                </FormGroup>
+                <TextBox label={'Имя пользователя'}
+                         value={this.state.user.username}
+                         onChange={this.handleChange.bind(this, 'username')}/>
 
-                <FormGroup controlId="formHorizontalRoles">
-                    <Col componentClass={ControlLabel} sm={1}>Roles</Col>
-                    <Col sm={2}>
-                        <FormControl componentClass="select" multiple>
-                            <option value="select">select roles</option>
-                            <option value="other">...</option>
-                        </FormControl>
-                    </Col>
-                </FormGroup>
+                <TextBox label={'Пароль'}
+                         type={"password"}
+                         value={this.state.user.password}
+                         onChange={this.handleChange.bind(this, 'password')}/>
 
-                <FormGroup controlId="formHorizontalPrivileges">
-                    <Col componentClass={ControlLabel} sm={1}>Privileges</Col>
-                    <Col sm={2}>
-                        <FormControl componentClass="select" multiple>
-                            <option value="select">select privileges</option>
-                            <option value="other">...</option>
-                        </FormControl>
-                    </Col>
-                </FormGroup>
+                <SelectBox label={"Роли"}
+                           value={this.state.user.roles}
+                           onChange={this.handleChange.bind(this, 'roles')}/>
 
-                <FormGroup controlId="formHorizontalPrivileges">
+                <SelectBox label={"Права"}
+                           value={this.state.user.privileges}
+                           onChange={this.handleChange.bind(this, 'privileges')}/>
+
+                <FormGroup controlId="formHorizontalSubmit">
                     <Col sm={1}/>
-                    <Col componentClass={Button} sm={1}>Create user</Col>
+                    <Col componentClass={Button} sm={2} onClick={this.addUser}>Create user</Col>
                 </FormGroup>
             </Form>
         </div>
